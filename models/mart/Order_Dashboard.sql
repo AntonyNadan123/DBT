9ec27f5ac23d10e55dd4715086d982144aeded39
Antony Nadan <antonynadanbtl007@gmail.com>
2024-12-09 17:28:08 +0000

    test

--- models/mart/Order_Dashboard.sql
+++ models/mart/Order_Dashboard.sql
@@ -2,4 +2,4 @@ select cust.C_CUSTKEY,sum(O_TOTALPRICE) as O_TOTALPRICE
 from {{ ref('stg_customer') }} cust
 join {{ ref('stg_orders') }} ord
 on cust.C_CUSTKEY = ord.O_CUSTKEY
-group by cust.C_CUSTKEY
+group by cust.C_CUSTKEY 
