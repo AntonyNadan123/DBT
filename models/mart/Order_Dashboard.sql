cc0711b14955199dcb0a4ae298f3ae85d77aada4
Antony Nadan <antonynadanbtl007@gmail.com>
2024-12-09 16:01:42 +0000

    c_custkey checkin

--- models/mart/Order_Dashboard.sql
+++ models/mart/Order_Dashboard.sql
@@ -1,5 +1,5 @@
-select C_CUSTKEY,sum(O_TOTALPRICE) as O_TOTALPRICE
+select cust.C_CUSTKEY,sum(O_TOTALPRICE) as O_TOTALPRICE
 from {{ ref('stg_customer') }} cust
 join {{ ref('stg_orders') }} ord
 on cust.C_CUSTKEY = ord.O_CUSTKEY
-group by C_CUSTKEY
+group by cust.C_CUSTKEY
